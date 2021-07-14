c1ebee076546ee435dd7c1dc6cf34a842b887e68
leezeeyee <easilylazy@qq.com>
2021-07-14 11:01:17 +0800

    finish backward of maxpooling

--- homework-12345/homework3/layers/pooling_layer.py
+++ homework-12345/homework3/layers/pooling_layer.py
@@ -34,10 +34,10 @@ class MaxPoolingLayer():
 			input_after_pad = np.pad(Input, ((0,), (0,), (self.pad,), (self.pad,)), mode='constant', constant_values=0)
 
 			b_str,c_str,h_str,w_str=input_after_pad.strides
-			# strides2=(c.strides[0]*k.shape[0],k.strides[0],c.strides[0],c.strides[1],)#c.strides[2],c.strides[1],c.strides[2])
-
+			self.height_o=int(self.height / self.kernel_size)
+			self.width_o=int(self.width / self.kernel_size)
 			strides=(b_str,c_str,h_str*self.kernel_size,int(w_str/self.width)*self.kernel_size,h_str,w_str)
-			shape = (self.batch_size, self.channels, int(self.height / self.kernel_size), int(self.width / self.kernel_size), self.kernel_size, self.kernel_size)
+			shape = (self.batch_size, self.channels, self.height_o, self.width_o, self.kernel_size, self.kernel_size)
 			return np.lib.stride_tricks.as_strided(input_after_pad,shape=shape,strides=strides).max(axis=(-2,-1))
 		except:
 			import pdb 
@@ -54,6 +54,21 @@ class MaxPoolingLayer():
 		############################################################################
 	    # TODO: Put your code here
 		# Calculate and return the new delta.
-		pass
+		self.batch_size,self.channels,self.height,self.width=self.Input.shape
+		input_after_pad = np.pad(self.Input, ((0,), (0,), (self.pad,), (self.pad,)), mode='constant', constant_values=0)
+		b_str,c_str,h_str,w_str=input_after_pad.strides
+		strides=(b_str,c_str,h_str*self.kernel_size,int(w_str/self.width)*self.kernel_size,h_str,w_str)
+
+		shape = (self.batch_size, self.channels, self.height_o, self.width_o,self.kernel_size, self.kernel_size)
+
+		block = np.lib.stride_tricks.as_strided(input_after_pad,shape=shape,strides=strides)
+
+		input_reshape=block.reshape((self.batch_size*self.channels* self.height_o* self.width_o, self.kernel_size* self.kernel_size))
+		
+		input_arg=input_reshape.argmax(axis=1)
+		input_zero=np.zeros(input_reshape.shape)
+		input_zero[np.indices(input_arg.shape),input_arg]=1
+
+		return np.multiply(input_zero.reshape(self.batch_size, self.channels, self.height_o, self.width_o,self.kernel_size* self.kernel_size),delta[:,:,:,:,None]).reshape(input_after_pad.shape)
 
 	    ############################################################################
