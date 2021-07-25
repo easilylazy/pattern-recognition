267ab2a32812f58f9e47cd602196d4a7749afa31
leezeeyee <easilylazy@qq.com>
2021-07-25 19:41:15 +0800

    add exp

--- homework-12345/homework-2/homework2-mlp/criterion/softmax_cross_entropy.py
+++ homework-12345/homework-2/homework2-mlp/criterion/softmax_cross_entropy.py
@@ -2,51 +2,55 @@
 
 from warnings import resetwarnings
 import numpy as np
+
 # a small number to prevent dividing by zero, maybe useful for you
 EPS = 1e-11
-def softmax(input):
-	batch_size=input.shape[0]
-	class_num=input.shape[1]
-	sum_data=np.sum(input,axis=1)
-	sum_data_exp=sum_data.reshape(batch_size,1)*np.ones((1,class_num))
-	return input/sum_data_exp
 
-class SoftmaxCrossEntropyLossLayer():
-	def __init__(self):
-		self.acc = 0.
-		self.loss = np.zeros(1, dtype='f')
 
-	def forward(self, logit, gt):
-		"""
+def softmax(Input):
+    batch_size = Input.shape[0]
+    class_num = Input.shape[1]
+    data_exp = np.exp(Input)
+    sum_data_exp = np.sum(data_exp, axis=1)
+    sum_data_exp_reshape = sum_data_exp.reshape(batch_size, 1) * np.ones((1, class_num))
+    return data_exp / sum_data_exp_reshape
+
+
+class SoftmaxCrossEntropyLossLayer:
+    def __init__(self):
+        self.acc = 0.0
+        self.loss = np.zeros(1, dtype="f")
+
+    def forward(self, logit, gt):
+        """
 	      Inputs: (minibatch)
 	      - logit: forward results from the last FCLayer, shape(batch_size, 10)
 	      - gt: the ground truth label, shape(batch_size, 10)
 	    """
 
-		############################################################################
-	    # TODO: Put your code here
-		# Calculate the average accuracy and loss over the minibatch, and
-		# store in self.accu and self.loss respectively.
-		# Only return the self.loss, self.accu will be used in solver.py.
-		self.logit=logit
-		self.gt=gt
-		self.res=softmax(logit)
-		test_pred=np.argmax(logit,axis=1)
-		test_true=np.argmax(gt,axis=1)
-		err=(test_pred-test_true)
-		self.acc = err[err==0].size/logit.shape[0]
-		self.loss = -np.sum(np.multiply(gt,np.log(self.res)))/logit.shape[0]
+        ############################################################################
+        # TODO: Put your code here
+        # Calculate the average accuracy and loss over the minibatch, and
+        # store in self.accu and self.loss respectively.
+        # Only return the self.loss, self.accu will be used in solver.py.
+        self.logit = logit
+        self.gt = gt
+        self.res = softmax(logit)
+        test_pred = np.argmax(logit, axis=1)
+        test_true = np.argmax(gt, axis=1)
+        err = test_pred - test_true
+        self.acc = err[err == 0].size / logit.shape[0]
+        self.loss = -np.sum(np.multiply(gt, np.log(self.res + EPS))) / logit.shape[0]
 
-	    ############################################################################
+        ############################################################################
 
-		return self.loss
+        return self.loss
 
+    def backward(self):
 
-	def backward(self):
+        ############################################################################
+        # TODO: Put your code here
+        # Calculate and return the gradient (have the same shape as logit)
+        return self.res - self.gt  # np.multiply(self.gt,(self.res-1))
 
-		############################################################################
-	    # TODO: Put your code here
-		# Calculate and return the gradient (have the same shape as logit)
-		return self.res-self.gt#np.multiply(self.gt,(self.res-1))
-
-	    ############################################################################
+        ############################################################################
