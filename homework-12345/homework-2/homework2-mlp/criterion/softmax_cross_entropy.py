fix softmax error

homework-12345/homework-2/homework2-mlp/criterion/softmax_cross_entropy.py
@@ -1,10 +1,11 @@
 """ Softmax Cross-Entropy Loss Layer """
 
+from warnings import resetwarnings
 import numpy as np
 from layers.sigmoid_layer import sigmoid
 # a small number to prevent dividing by zero, maybe useful for you
 EPS = 1e-11
-
+ 
 
 
 class SoftmaxCrossEntropyLossLayer():
@@ -26,11 +27,12 @@ class SoftmaxCrossEntropyLossLayer():
 		# Only return the self.loss, self.accu will be used in solver.py.
 		self.logit=logit
 		self.gt=gt
+		self.res=sigmoid(logit)
 		test_pred=np.argmax(logit,axis=1)
 		test_true=np.argmax(gt,axis=1)
 		err=(test_pred-test_true)
 		self.acc = err[err==0].size/logit.shape[0]
-		self.loss = -np.sum(np.multiply(gt,np.log(sigmoid(logit))))/logit.shape[0]
+		self.loss = -np.sum(np.multiply(gt,np.log(self.res)))/logit.shape[0]
 
 	    ############################################################################
 
@@ -42,6 +44,6 @@ class SoftmaxCrossEntropyLossLayer():
 		############################################################################
 	    # TODO: Put your code here
 		# Calculate and return the gradient (have the same shape as logit)
-		return np.multiply(self.gt,(sigmoid(self.logit)-1))
+		return self.logit-self.gt#np.multiply(self.gt,(self.res-1))
 
 	    ############################################################################

homework-12345/homework-2/homework2-mlp/test.py
@@ -39,7 +39,8 @@ disp_freq = 50
 
 from criterion import EuclideanLossLayer
 from criterion import SoftmaxCrossEntropyLossLayer
-
+# from tf.losses import softmax_cross_entropy
+# criterion = tf.losses.softmax_cross_entropy()
 criterion = SoftmaxCrossEntropyLossLayer()
 from optimizer import SGD
 
@@ -66,11 +67,11 @@ from layers import ReLULayer
 reluMLP = Network()
 # TODO build ReLUMLP with FCLayer and ReLULayer
 reluMLP.add(FCLayer(784, 128))
-reluMLP.add(ReLULayer())
+# reluMLP.add(ReLULayer())
 reluMLP.add(FCLayer(128, 10))
 
 reluMLP, relu_loss, relu_acc = train(reluMLP, criterion, sgd, data_train, max_epoch, batch_size, disp_freq)
 
 test(reluMLP, criterion, data_test, batch_size, disp_freq)
 
-plot_loss_and_acc({                   'relu': [relu_loss, relu_acc]})
+plot_loss_and_acc({                   'relu': [relu_loss, relu_acc]},title='test_sys_soft')