8ac660e24893ad81dc64be81f7658b11a7e7ce18
leezeeyee <easilylazy@qq.com>
2021-08-10 22:54:40 +0800

    enable gpu

--- homework-12345/homework4/train.py
+++ homework-12345/homework4/train.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 # %%
-mnist_dataset = input_data.read_data_sets("..\homework-2\MNIST_data", one_hot=True)
+mnist_dataset = input_data.read_data_sets("MNIST_data", one_hot=True)
 
 
 # %%
@@ -22,6 +22,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+pngname = "convnet2fc3_dr_lr_"
+drop_out=0.3
+epochs = 50
 
 class Net(nn.Module):
     def __init__(self):
@@ -32,22 +37,24 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(8, 16, 3, 1, 1)
         # an affine operation: y = Wx + b
         self.fc1 = nn.Linear(16 * 7 * 7, 128)  # 7*7 from image dimension
-        # self.fc2 = nn.Linear(128, 84)
-        self.fc3 = nn.Linear(128, 10)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 10)
+        self.fc_dropout = nn.Dropout(drop_out) 
 
     def forward(self, x):
         # Max pooling over a (2, 2) window
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
         # If the size is a square, you can specify with a single number
+        x = self.fc_dropout(x)
         x = F.max_pool2d(F.relu(self.conv2(x)), 2)
         x = torch.flatten(x, 1)  # flatten all dimensions except the batch dimension
         x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
+        x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
 
-net = Net()
+net = Net().to(device)
 try:
     torch.cuda.empty_cache()  # PyTorch thing
     print("success clean")
@@ -60,9 +67,9 @@ def train_loop(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
         # Compute prediction and loss
-        X = X.reshape(X.shape[0], 1, 28, 28)
+        X = X.reshape(X.shape[0], 1, 28, 28).to(device)
         pred = model(X)
-        loss = loss_fn(pred, y.float())
+        loss = loss_fn(pred, y.to(device).float())
 
         # Backpropagation
         optimizer.zero_grad()
@@ -81,7 +88,8 @@ def test_loop(dataloader, model, loss_fn):
 
     with torch.no_grad():
         for X, y in dataloader:
-            X = X.reshape(X.shape[0], 1, 28, 28)
+            X = X.reshape(X.shape[0], 1, 28, 28).to(device)
+            y = y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y.float()).item()
             correct += (pred.argmax(1) == y.argmax(1)).type(torch.float).sum().item()
@@ -104,7 +112,7 @@ learning_rate = 0.1
 optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate)
 
 # %%
-model_load = net
+model_load = net.to(device)
 try:
     model_load.load_state_dict(torch.load("model_weights.pth"))
     model_load.eval()
@@ -114,7 +122,6 @@ except:
 # %%
 test_loop(test_dataloader, model_load, loss_fn)
 SAVE_CKP = False
-epochs = 20
 avg_test_loss, avg_test_acc = [], []
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
@@ -122,14 +129,14 @@ for t in range(epochs):
     test_loss, test_acc = test_loop(test_dataloader, model_load, loss_fn)
     avg_test_loss.append(test_loss)
     avg_test_acc.append(test_acc)
-    filename = "checkpoint\model_w_epoch" + str(t) + ".pth"
+    filename = "checkpoint\model_w_epoch" + pngname + str(t) + ".pth"
     if SAVE_CKP:
         torch.save(model_load.state_dict(), filename)
         print("save in " + filename)
-_,accu=test_loop(test_dataloader, model_load, loss_fn)
+loss,accu=test_loop(test_dataloader, model_load, loss_fn)
 from plot import plot_loss_and_acc
 
-filename = "convnet2fc_lr_" + str(learning_rate)+'_acc_'+str(accu)
+filename = pngname+str(learning_rate)+'_epo_'+str(epochs)+'_acc_'+str(accu)+'_loss_'+str(loss)
 plot_loss_and_acc({"ConvNet": [avg_test_loss, avg_test_acc]}, filename=filename)
 
 print("Done!")
