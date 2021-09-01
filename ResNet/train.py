c374de60adb67c54ebc0afc5187ee03f77bb532b
leezeeyee <easilylazy@qq.com>
2021-09-01 14:45:39 +0800

    enable first test

--- ResNet/train.py
+++ ResNet/train.py
@@ -10,11 +10,20 @@ import numpy as np
 # %%
 mnist_dataset = input_data.load_data(path='data/cifar-10-batches-py',one_hot=True)
 
+
+# %%
+# params
+pngname = "resnet_lr_"
+drop_out=0.3
+epochs = 10
+batch_size=128
+learning_rate = 0.1
+
 # %%
 from torch.utils.data import DataLoader
 
-train_dataloader = DataLoader(mnist_dataset.train, batch_size=64, shuffle=True)
-test_dataloader = DataLoader(mnist_dataset.test, batch_size=64, shuffle=True)
+train_dataloader = DataLoader(mnist_dataset.train, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(mnist_dataset.test, batch_size=batch_size, shuffle=True)
 
 # %%
 import torch
@@ -23,37 +32,48 @@ import torch.nn.functional as F
 
 device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-pngname = "convnet2fc3_dr_lr_"
-drop_out=0.3
-epochs = 10
+
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # 1 input image channel, 6 output channels, 7x7 square convolution
         # kernel
-        self.conv1 = nn.Conv2d(3, 8, 3, 1, 1)
+        self.conv1_0 = nn.Conv2d(3, 16, 3, 1, 1)
+        self.conv1 = nn.Conv2d(16, 16, 3, 1, 1)
+        self.conv2_0 = nn.Conv2d(16, 32, 3, 2, 1)
+        self.conv2 = nn.Conv2d(32, 32, 3, 1, 1)
+        self.conv3_0 = nn.Conv2d(32, 64, 3, 2, 1)
+        self.conv3 = nn.Conv2d(64, 64, 3, 1, 1)
 
-        self.conv_res = nn.Conv2d(8, 8, 3, 1, 1)
-        
-        self.conv2 = nn.Conv2d(8, 16, 3, 1, 1)
-        # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 8 * 8, 128)  # 7*7 from image dimension
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 10)
+        self.pool=nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(64,10)  
         self.fc_dropout = nn.Dropout(drop_out) 
 
     def forward(self, x):
         # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        x2_1 = F.relu(self.conv_res(x))
-        # If the size is a square, you can specify with a single number
-        # x = self.fc_dropout(x)
-        x = F.max_pool2d(F.relu(self.conv2(x+x2_1)), 2)
+        x1_0 = F.relu(self.conv1_0(x))
+        x = F.relu(self.conv1(x1_0))
+        x = F.relu(self.conv1(x))
+
+        x2_0=x1_0+x
+        x = F.relu(self.conv2_0(x2_0))
+        x2_1 = F.relu(self.conv2(x))
+        x = F.relu(self.conv2(x2_1))
+        x = F.relu(self.conv2(x))
+
+        x3_0=x2_1+x
+        x = F.relu(self.conv3_0(x3_0))
+        x = F.relu(self.conv3(x))
+
+        # print(x.shape)
+
+        x = self.pool(x)
+        # print(x.shape)
+        # x = F.relu(self.conv2(x))
+        # print(x.shape)
         x = torch.flatten(x, 1)  # flatten all dimensions except the batch dimension
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc(x)
         return x
 
 
@@ -110,9 +130,8 @@ import torch.optim as optim
 
 loss_fn = nn.MSELoss()
 # nn.CrossEntropyLoss()
-learning_rate = 0.1
 # for learning_rate in range(0.01)
-optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate,weight_decay=0.0001,momentum=0.9)
 
 # %%
 model_load = net.to(device)
