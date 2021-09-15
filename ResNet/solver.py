8ec502cad480be0dd85162cb62de8752234ee403
leezeeyee <easilylazy@qq.com>
2021-09-15 15:31:07 +0800

    🐛 FIX: no_sync step is invalid

--- ResNet/solver.py
+++ ResNet/solver.py
@@ -86,19 +86,21 @@ def train_loop(dataloader, model, loss_fn, optimizer, rank):
         # Compute prediction and loss
 
         X, y = X.to(rank), y.to(rank)
-        my_context = model.no_sync if rank != -1 and batch % num_batches != 0 else nullcontext
-        with my_context():
-            pred = model(X)
-            loss = loss_fn(pred, y)
-            loss.backward()
-        if batch % num_batches == 0:
-            optimizer.step()
-            optimizer.zero_grad()
+        # my_context = model.no_sync if rank != -1 and batch % num_batches != 0 else nullcontext
+        # with my_context():
+        pred = model(X)
+        loss = loss_fn(pred, y)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        # if batch % num_batches == 0:
+        #     optimizer.step()
+        #     optimizer.zero_grad()
 
         train_loss += loss.item()
         correct += (pred.data.argmax(1) == y).type(torch.int).sum().item()
 
-        if batch % 100 == 0:
+        if rank == 0:
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
     train_loss /= num_batches
@@ -121,7 +123,8 @@ def test_loop(dataloader, model, loss_fn, rank):
 
     test_loss /= num_batches
     correct /= size
-    print(
+    if rank==0:
+        print(
         f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
-    )
+        )
     return test_loss, correct
